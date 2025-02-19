4d79df24d8a5a71104ad55adf2fa6111f2f87836
priti-thinkitive <priti.talekar@thinkitve.com>
2025-02-19 13:05:37 +0530

    Initial commit with project setup

---
+++ FirstDemoProject/src/main/java/com/pritithink/FirstDemoProject/FirstDemoProjectApplication.java
@@ -0,0 +1,14 @@
+package com.pritithink.FirstDemoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDemoProjectApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("hey");
+	}
+
+}
