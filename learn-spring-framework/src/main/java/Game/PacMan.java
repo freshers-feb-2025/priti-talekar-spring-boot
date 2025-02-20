aa4a7b0128957dfb783ca44f2be0ebeb626306bf
priti-thinkitive <priti.talekar@thinkitve.com>
2025-02-20 19:50:44 +0530

    Spring Framework Day2

---
+++ learn-spring-framework/src/main/java/Game/PacMan.java
@@ -0,0 +1,26 @@
+package Game;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PacMan implements GammingConsole{
+    @Override
+    public void Up() {
+        System.out.println("up");
+    }
+
+    @Override
+    public void down() {
+        System.out.println("down");
+    }
+
+    @Override
+    public void left() {
+        System.out.println("left");
+    }
+
+    @Override
+    public void right() {
+        System.out.println("right");
+    }
+}
