3ab3d0e8b06a46551ac18b43a23c923d5807773b
priti-thinkitive <priti.talekar@thinkitve.com>
2025-02-27 17:35:30 +0530

    RestAPI crud with MongoDB

---
+++ MongodbCrud/src/main/java/com/thinkiitve/MongodbCrud/DTO/StudentRequestDTO.java
@@ -0,0 +1,34 @@
+package com.thinkiitve.MongodbCrud.DTO;
+
+public class StudentRequestDTO {
+
+    private String studentName;
+    private String studentCity;
+    private String studentCollege;
+
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentCity() {
+        return studentCity;
+    }
+
+    public void setStudentCity(String studentCity) {
+        this.studentCity = studentCity;
+    }
+
+    public String getStudentCollege() {
+        return studentCollege;
+    }
+
+    public void setStudentCollege(String studentCollege) {
+        this.studentCollege = studentCollege;
+    }
+}
+
