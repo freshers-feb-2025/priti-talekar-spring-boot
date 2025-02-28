763ea596c82337c9bed9b5fe8f711d5e15d87606
priti-thinkitive <priti.talekar@thinkitve.com>
2025-03-01 01:47:10 +0530

    Remove unnecessary files from git tracking

--- MongodbCrud/src/main/java/com/thinkiitve/MongodbCrud/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thinkiitve.MongodbCrud.repository;
-
-import com.thinkiitve.MongodbCrud.Entity.Student;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StudentRepository extends MongoRepository<Student, ObjectId> {
-
-}
