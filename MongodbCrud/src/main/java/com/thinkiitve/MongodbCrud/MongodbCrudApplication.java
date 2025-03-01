66683a115b437b145bed824ae2c45aa5bc247b6d
priti-thinkitive <priti.talekar@thinkitve.com>
2025-03-01 09:54:52 +0530

    app class added

--- MongodbCrud/src/main/java/com/thinkiitve/MongodbCrud/MongodbCrudApplication.java
+++ MongodbCrud/src/main/java/com/thinkiitve/MongodbCrud/MongodbCrudApplication.java
@@ -1,14 +1,14 @@
-package com.thinkiitve.MongodbCrud;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class MongodbCrudApplication {
-
-	public static void main(String[] args)
-	{
-		SpringApplication.run(MongodbCrudApplication.class);
-	}
-
-}
+//package com.thinkiitve.MongodbCrud;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class MongodbCrudApplication {
+//
+//	public static void main(String[] args)
+//	{
+//		SpringApplication.run(MongodbCrudApplication.class);
+//	}
+//
+//}
