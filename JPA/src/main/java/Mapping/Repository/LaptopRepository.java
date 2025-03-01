9e2f2214db365f141858f412e2c2426a2fed162f
priti-thinkitive <priti.talekar@thinkitve.com>
2025-03-01 14:09:53 +0530

    removed subprojects from staging

--- JPA/src/main/java/Mapping/Repository/LaptopRepository.java
+++
@@ -1,7 +0,0 @@
-package Mapping.Repository;
-
-import Mapping.Entity.Laptop;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LaptopRepository extends CrudRepository<Laptop,Integer> {
-}
--- RestAPIBasic/src/main/java/com/thinkitive/RestApiExtra/BookRepository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thinkitive.RestApiExtra.BookRepository;
-
-import com.thinkitive.RestApiExtra.Entity.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book,Integer> {
-}
